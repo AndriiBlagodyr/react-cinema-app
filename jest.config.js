19c6276e6d55b134ba4b6b9346bae79b614319c3
Mr_Anderson <andrii.blagodyr@gmail.com>
2020-07-03 11:46:25 +0300

    feat(AB-2)Babel, EsLint, Prettier, Jest setups

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+module.exports = {
+	rootDir: path.join(__dirname, '.'),
+	moduleDirectories: ['node_modules', path.join(__dirname, 'src'), 'public', __dirname],
+	collectCoverage: true,
+	collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}', '!src/**/index.js'],
+	coverageReporters: ['text-summary', 'json', 'html', 'lcovonly'],
+	coverageThreshold: {
+		global: {
+			branches: 100,
+			functions: 100,
+			statements: 100,
+			lines: 100,
+		},
+	},
+};
