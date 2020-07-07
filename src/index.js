b37935cedbb486d83886841c29515574eec996b9
AndriiBlagodyr <andrii.blagodyr@gmail.com>
2020-07-07 14:33:45 +0300

    Styled components examples

--- src/index.js
+++ src/index.js
@@ -1,13 +1,7 @@
-import 'react-app-polyfill/ie11';
-import 'react-app-polyfill/stable';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import './index.css';
 import App from 'components/App';
 
-ReactDOM.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
-	document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
